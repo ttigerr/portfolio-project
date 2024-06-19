2ac49769ce0b538402533bc49dae73c2fd8a7b3d
Tiger <tiger.chalernphon15@gmail.com>
2024-06-19 22:45:02 +1000

    About page updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,40 +3,72 @@ import React from "react";
 const About = () => { 
   return (
     <div name="about" id="about" className="h-screen bg-[#FAFAFA]">
-        <div className="m-screen flex flex-col justify-center items-center h-full">
-            {/* First Container "About" title */}
-            <div className="pb-20">
-                <strong className="text-4xl md:text-7xl">
-                    About
-                </strong>
-            </div>
-            {/* Second container for the content */}
-            <div className="flex flex-row justify-center items-center gap-5">
+        <div className="m-screen flex flex-col justify-center h-full">
+            {/* Top container */}
+            <div className="flex flex-row mb-20">
                 <div className="basis-6/12">
-                    <strong className="text-2xl md:text-4xl ">
-                        Hey there! <br/>
+                    <strong className="text-xl md:text-3xl">
+                        ABOUT ME.
                     </strong>
-                    <br/>
-                    <p>
-                        My name is Tiger. I'm a Front-end developer in both Website and Mobile Application. based in Sydney.
-                        I graduated from the Academy of Interactive Technology with a Bachelor of Information Technology specializing in Mobile Application Development in October 2022.
-                    </p>
                 </div>
                 <div className="basis-6/12">
-                    <strong className="text-2xl md:text-4xl ">
-                        Working Experince
+                    <strong className="text-xl md:text-3xl">
+                    " 
+                                I graduated from the Academy of Interactive Technology with a Bachelor of Information Technology specializing in Mobile Application Development " 
                     </strong>
+                </div>
+            </div>
+            {/* Bottom container */}
+            <div className="flex flex-row gap-5">
+                <div className="basis-6/12">
+                    <strong className="text-2xl md:text-7xl ">
+                        RELEVANT <br/> EXPERIENCE 
+                    </strong>
+                </div>
+                <div className="basis-6/12">
+
                     {/* Working experience containers */}  
                     <div className="flex flex-col">
-                        <div className="bg-white my-10">
-                            <p>SHORTSTOP</p>
+                        <div className="">
+                            <strong className="text-xl md:text-3xl">
+                                SHORTSTOP
+                            </strong>
                             <br/>
-                            <p>Digital Content Manager</p>
+                            <br/>
+                            <div className="flex flex-row justify-between">
+                                <strong className="text-xl md:text-xl">
+                                    DIGITAL CONTENT MANAGER
+                                </strong>
+                                <strong className="text-xl md:text-xl">
+                                    SEP 2023 - PRESENT
+                                </strong>
+                            </div>
+                            <br/>
+                            <p>
+                                My name is Tiger. I'm a Front-end developer in both Website and Mobile Application. based in Sydney.
+                                I graduated from the Academy of Interactive Technology with a Bachelor of Information Technology.
+                            </p>
                         </div>
-                        <div className="bg-white">
-                            <p>INTELLIGENCE PROFIT SOLUTIONS</p>
+                        <div className="my-10 border-[0.5px] border-gray-400"></div>
+                        <div>
+                            <strong className="text-xl md:text-3xl">
+                                INTELLIGENT PROFIT SOLUTIONS
+                            </strong>
                             <br/>
-                            <p>Digital Content Manager</p>
+                            <br/>
+                            <div className="flex flex-row justify-between">
+                                <strong className="text-xl md:text-xl">
+                                    UX / UI DESIGNER
+                                </strong>
+                                <strong className="text-xl md:text-xl">
+                                    APR 2022 - DEC 2022
+                                </strong>
+                            </div>
+                            <br/>
+                            <p>
+                                My name is Tiger. I'm a Front-end developer in both Website and Mobile Application. based in Sydney.
+                                I graduated from the Academy of Interactive Technology with a Bachelor of Information Technology
+                            </p>
                         </div>
                     </div>               
                 </div>
